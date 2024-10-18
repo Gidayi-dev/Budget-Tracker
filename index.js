312a25027ba75e8d6125c8a509e125e2a50b2d98
Gidayi Millyannah <millyannahi@gmail.com>
2024-10-18 05:55:21 +0300

    add

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import fs from 'fs';
 import { Command } from "commander";
 
 const program = new Command();
@@ -12,14 +13,25 @@ program.command("new")
     const title = options.title;
     const body = options.body;
     
-    const newNote = {
+    const newBudget = {
         title: title,
         body: body,
         createdAt: new Date(),
         lastUpdateAt: new Date(),
     }
     
-})
+    const loadedBudget = fs.readFileSync("./data/budgets.json", "utf-8");
+    let budget;
+    if (!loadedBudget) {
+        budget = [];
+    }
+    budget = JSON.parse(loadedBudget);
+
+    budget.push(newBudget);
+
+    fs.writeFileSync("./data/budgets.json", JSON.stringify(budget));
+});
+
 
 
 program.parse(process.argv);
